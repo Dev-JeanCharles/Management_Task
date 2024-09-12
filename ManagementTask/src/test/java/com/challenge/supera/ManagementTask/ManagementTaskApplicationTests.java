5c3ee7b400d5b0d867b1f34c9f588bbb597de746
Dev-JeanCharles <jeancduasan@gmail.com>
2024-09-11 21:42:00 -0300

    feat: start

---
+++ ManagementTask/src/test/java/com/challenge/supera/ManagementTask/ManagementTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.supera.ManagementTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
