dbdc7c05a3be2fe59806c467510e70d38fa72d2e
Dev-JeanCharles <jeancduasan@gmail.com>
2024-09-14 00:17:59 -0300

    feat: Swagger + Operações no ItemController e TarefaController

--- ManagementTask/src/main/java/com/challenge/supera/ManagementTask/application/web/controllers/ItemController.java
+++ ManagementTask/src/main/java/com/challenge/supera/ManagementTask/application/web/controllers/ItemController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/itens")
+@RequestMapping("/item")
 public class ItemController {
 
     private final ItemService service;
